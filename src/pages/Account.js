e939e737c0cc3b61743f6bcefe18aecb9521264e
DELTA18 <rajmalave4@gmail.com>
2024-04-20 00:00:11 +0530

    started working on account page

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import '../styles/account.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import avatar from '../assets/avatar.png'
 const Account = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
@@ -12,7 +14,15 @@ const Account = () => {
     <>
     <div className='sep'>
     <Header user_name={user_name} user_id={user_id} />
-    <h1>Account page</h1>
+    <section className='acc_container space_giver hidden'>
+      <div className='acc_left'>
+        <div className='user_avatar'> <img src={avatar}></img> </div>
+        <div className='acc_user'>raj </div>
+        <div className='acc_user'> sdfgh34567</div>
+        <div className='acc_user'> 123456789</div>
+      </div>
+      <div className='acc_right'>r</div>  
+    </section>
     <Footer/>
     </div>
     </>
